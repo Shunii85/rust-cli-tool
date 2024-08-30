3c6d111c3764b5b6e7ef5b49539888719b0d4f12
shunii <shunii85@outlook.jp>
2024-08-30 09:55:49 +0900

    nicer error reporting with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -6,8 +7,17 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
-    println!("pattern: {:?}, path {:?}", args.pattern, args.path);
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
